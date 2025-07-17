cdb49b0efaeda7b7ae4bf29cd9cbb2ea9fe7472f
seagetch <sigetch@gmail.com>
2025-07-17 21:35:00 +0900

    aivisspeech can switch cpu / nvidia mode by DEVICE variable.

--- 00_aivisspeech_docker.sh
+++ 00_aivisspeech_docker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-docker pull ghcr.io/aivis-project/aivisspeech-engine:cpu-latest
-docker run --rm -p '10101:10101' \
+set -x
+if [ ! -n "$DEVICE" ]; then
+  DEVICE=cpu
+fi
+
+if [ "$DEVICE" != "cpu" ]; then
+  OPT="--gpus all"
+else
+  OPT=""
+fi
+docker pull ghcr.io/aivis-project/aivisspeech-engine:$DEVICE-latest
+docker run --rm $OPT -p '10101:10101' \
   -v ~/.local/share/AivisSpeech-Engine:/home/user/.local/share/AivisSpeech-Engine-Dev \
-  ghcr.io/aivis-project/aivisspeech-engine:cpu-latest
+  ghcr.io/aivis-project/aivisspeech-engine:$DEVICE-latest
